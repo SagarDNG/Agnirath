71336ffb9af4ccca0b0a2556301d22e9e8a75151
SagarDNG <sagardng123@gmail.com>
2022-12-15 03:33:51 +0530

    mobile css temp

--- src/App.js
+++ src/App.js
@@ -12,16 +12,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <About />
-      <Vision />
-      <Team />
-      <Competitions />
-      <Sponsers />
-      <Contact />
-    </div>
+    <>
+
+      <div className="App">
+        <Navbar />
+        <Home />
+        <About />
+        <Vision />
+        <Team />
+        <Competitions />
+        <Sponsers />
+        <Contact />
+      </div>
+      <div className="mobile-css">
+        <h1>Team Agnirath, IITM</h1>
+        <h3>A solar car competitive team, IIT Madras, India</h3>
+        <p>Kindly view it in PC, Site will soon be mobile friendly too...</p>
+      </div>
+    </>
   );
 }
 
