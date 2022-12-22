dbf69a297c5e41a8c8b73f2f2d16add1d105baed
SagarDNG <sagardng123@gmail.com>
2022-12-23 00:35:55 +0530

    vision added

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -13,9 +13,24 @@ function Vision() {
             <div className="col-3">
                 <img data-aos="fade-right" src={Logo} alt="" className='visionLogo' />
             </div>
-            <div className='vision col-8 text-center'>
-                {/* https://stackoverflow.com/questions/17629286/css-vertical-line-between-bullets-in-an-unordered-list/17629815#17629815 */}
-                Vision of the team here
+            <div className='vision col-8 d-flex justify-content-around'>
+                {/* https://stackoverflow.com/questions/17629286/css-vertical-line-between-bullets-in-an-unordered-list/17629815#17629815
+                Vision of the team here */}
+                <ul>
+                    <li>Commercial Solar car</li>
+                    <li>Solar EV race team</li>
+                    <li>Commercialization and affordability</li>
+                    <li>Reliability</li>
+                    <li>Sustainability</li>
+                    <li>Endurance</li>
+                </ul>
+                <ul>
+                    <li>Sustainability</li>
+                    <li>Inclusivity</li>
+                    <li>Fostering talent</li>
+                    <li>Renewables R&D</li>
+                    <li>Peak Efficiencies</li>
+                </ul>
             </div>
         </div>
     )
